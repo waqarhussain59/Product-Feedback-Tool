0ed21fadd0e79e46328938283642ea382a836d71
waqar hussain <whussain1359@gmail.com>
2023-12-29 12:36:35 +0500

    Changes in Master blade

--- resources/views/layouts/mater.blade.php
+++ resources/views/layouts/mater.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Your Name">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Feedback tool</title>
+    <title>Product Feedback Tool</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
